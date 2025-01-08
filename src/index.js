f84591aeffcd359fb36dbe47a24b98622189cc25
DanHult88 <144914317+DanHult88@users.noreply.github.com>
2025-01-08 10:32:40 +0100

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Note the import from 'react-dom/client'
-import App from './App';
-import './index.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root')); // Create root
-root.render(<App />); // Render the app
